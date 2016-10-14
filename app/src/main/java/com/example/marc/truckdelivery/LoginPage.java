57fdab60a427953e0a21c52a4ac7337bc3ffc14d
jahmarc <marc@datanet-ch.com>
2016-10-14 09:52:54 +0200

    test2

--- app/src/main/java/com/example/marc/truckdelivery/LoginPage.java
+++ app/src/main/java/com/example/marc/truckdelivery/LoginPage.java
@@ -9,6 +9,6 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-        //test
+        //test 2
     }
 }
