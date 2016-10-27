ff0bb2b17d3c00a9d259e8f82714440fdd687ad1
jahmarc <marc@datanet-ch.com>
2016-10-27 11:11:14 +0200

    ajout page new driver, customer, delivery

---
+++ app/src/main/java/com/example/marc/truckdelivery/Delivery.java
@@ -0,0 +1,13 @@
+package com.example.marc.truckdelivery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Delivery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delivery);
+    }
+}
