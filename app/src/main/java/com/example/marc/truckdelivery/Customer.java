8d93be93ac4f5a72e6f5105d7f791aa2716fa287
jahmarc <marc@datanet-ch.com>
2016-11-04 09:05:13 +0100

    menu qui s'affiche toujours pas

--- app/src/main/java/com/example/marc/truckdelivery/Customer.java
+++ app/src/main/java/com/example/marc/truckdelivery/Customer.java
@@ -2,6 +2,7 @@ package com.example.marc.truckdelivery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Customer extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class Customer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/marc/truckdelivery/LoginPage.java
+++ app/src/main/java/com/example/marc/truckdelivery/LoginPage.java
@@ -1,7 +1,10 @@
 package com.example.marc.truckdelivery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -11,4 +14,25 @@ public class LoginPage extends AppCompatActivity {
         setContentView(R.layout.activity_login_page);
         //test 2
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        Intent intent = getIntent();
+        switch(item.getItemId()) {
+            case R.id.app_bar_search:
+
+            case R.id.app_bar_language:
+
+        }
+        return true;
+        }
+
 }
