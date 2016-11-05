72c7c4f7feba593a73f5700cb4e2af2cdb880fd0
Helder <Osheed_@hotmail.com>
2016-11-05 09:12:03 +0100

    menuBar 1erePage

--- app/src/main/java/com/example/marc/truckdelivery/admin_page.java
+++ app/src/main/java/com/example/marc/truckdelivery/admin_page.java
@@ -2,6 +2,7 @@ package com.example.marc.truckdelivery;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class admin_page extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class admin_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_page);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
