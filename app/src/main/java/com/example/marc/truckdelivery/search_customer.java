442b9fdd5be3775a69c75da54bd77f8d236ca09b
Helder <Osheed_@hotmail.com>
2016-11-11 09:58:09 +0100

    u2

--- app/src/main/java/com/example/marc/truckdelivery/search_customer.java
+++ app/src/main/java/com/example/marc/truckdelivery/search_customer.java
@@ -1,59 +1,41 @@
 package com.example.marc.truckdelivery;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class admin_page extends AppCompatActivity {
+public class search_customer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_page);
-
-        getSupportActionBar().setHomeButtonEnabled(false);
+        setContentView(R.layout.activity_search_customer);
+        getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
         getSupportActionBar().setLogo(R.drawable.ic_launcher);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF6C7CE2")));
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true; //prends le style pour le menu de menu_basic
+    }
 
-    }
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_basic,menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+    @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
 
         switch(item.getItemId()) {
-
+            case R.id.app_bar_search:
 
             case R.id.app_bar_language:
 
         }
         return true;
-    }
-
-    public void toDelivery(View view) {
-        Intent toDelivery = new Intent(this,search_delivery.class);
-        startActivity(toDelivery);
-
-    }
-
-    public void toClient(View view) {
-        Intent toClient = new Intent(this,search_customer.class);
-        startActivity(toClient);
-
-    }
-
-    public void toDriver(View view) {
-        Intent toDriver = new Intent(this,search_driver.class);
-        startActivity(toDriver);
     }
 }
--- app/src/main/java/com/example/marc/truckdelivery/search_delivery.java
+++ app/src/main/java/com/example/marc/truckdelivery/search_delivery.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class search_driver extends AppCompatActivity {
+public class search_delivery extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_driver);
+        setContentView(R.layout.activity_search_delivery);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
@@ -20,17 +20,16 @@ public class search_driver extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF6C7CE2")));
     }
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_search, menu);
         return true; //prends le style pour le menu de menu_basic
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
 
-        switch(item.getItemId()) {
+        switch (item.getItemId()) {
             case R.id.app_bar_search:
 
             case R.id.app_bar_language:
