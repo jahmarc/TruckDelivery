b2a6851faa367e2f40a100d2d86083ab9ce837a0
jahmarc <marc@datanet-ch.com>
2016-11-15 15:36:25 +0100

    Ajout de l'objet Delivery

--- app/src/main/java/db/object/Customer.java
+++ app/src/main/java/db/object/Customer.java
@@ -13,20 +13,28 @@ public class Customer {
     private String adress;
     private int postcode;
     private String locality;
+
     public int getId(){return id;}
     public void setId(int id){this.id = id;}
+
     public String getSociety(){return society;}
     public void setSociety(String society){this.society = society;}
+
     public String getName(){return name;}
     public void setName(String name){this.name = name;}
+
     public String getFirstname(){return firstname;}
     public void setFirstname(String firstname){this.firstname = firstname;}
+
     public String getPhone(){return phone;}
     public void setPhone(String phone){this.phone = phone;}
+
     public String getAdress(){return adress;}
     public void setAdress(String adress){this.adress = adress;}
+
     public int getPostcode(){return postcode;}
     public void setPostcode(int postcode){this.postcode = postcode;}
+
     public String getLocality(){return locality;}
     public void setLocality(String locality){this.locality = locality;}
 }
--- app/src/main/java/db/object/Driver.java
+++ app/src/main/java/db/object/Driver.java
@@ -12,18 +12,25 @@ public class Driver {
     private String plate;
     private String user;
     private String password;
+
     public int getId(){return id;}
     public void setId(int id){this.id = id;}
+
     public String getName(){return name;}
     public void setName(String name){this.name = name;}
+
     public String getFirstname(){return firstname;}
     public void setFirstname(String firstname){this.firstname = firstname;}
+
     public String getPhone(){return phone;}
     public void setPhone(String phone){this.phone = phone;}
+
     public String getPlate(){return plate;}
     public void setPlate(String plate){this.plate = plate;}
+
     public String getUser(){return user;}
     public void setUser(String user){this.user = user;}
+
     public String getPassword(){return password;}
     public void setPassword(String password){this.password = password;}
 
