8890ca14c54808d1a26d0f67bdecc88a12d1b9d6
Helder <Osheed_@hotmail.com>
2016-11-16 15:49:11 +0100

    3h pour trouver comment fonctionner la traduction

---
+++ app/src/main/java/com/example/marc/truckdelivery/LocaleHelper.java
@@ -0,0 +1,55 @@
+package com.example.marc.truckdelivery;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.preference.PreferenceManager;
+
+import java.util.Locale;
+
+/**
+ * Created by Helder on 10.11.2016.
+ */
+
+public class LocaleHelper {
+
+    private static final String SELECTED_LANG = "Locale.Helper.Selected.Language";
+
+    public static void OnCreate(Context context){
+        String lang = getPersistedData(context, Locale.getDefault().getLanguage());
+        setLocale(context,lang);
+    }
+    public static void OnCreate(Context context, String defaultLanguage){
+        String lang = getPersistedData(context, defaultLanguage);
+        setLocale(context,lang);
+    }
+
+    public static void setLocale(Context context, String lang) {
+        persist(context,lang);
+        updateResources(context,lang);
+    }
+    private static void persist(Context context, String lang){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(SELECTED_LANG,lang);
+        editor.apply();
+    }
+
+    private static void updateResources(Context context, String lang) {
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Resources resources = context.getResources();
+        Configuration config = resources.getConfiguration();
+        config.locale = locale;
+        resources.updateConfiguration(config,resources.getDisplayMetrics());
+    }
+
+    private static String getPersistedData(Context context, String language) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString(SELECTED_LANG,language);
+    }
+    public static String getLanguage(Context context){
+        return getPersistedData(context,Locale.getDefault().getLanguage());
+    }
+}
--- app/src/main/java/com/example/marc/truckdelivery/LoginPage.java
+++ app/src/main/java/com/example/marc/truckdelivery/LoginPage.java
@@ -1,21 +1,30 @@
 package com.example.marc.truckdelivery;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import java.util.Locale;
+import android.app.Activity;
+
+import com.example.marc.truckdelivery.LocaleHelper.*;
+
 
 public class LoginPage extends AppCompatActivity {
+    Context context;
 
-    //stocker la preference de langue serait une bonne idée avec des shared Preferences
-    //on saurait quelle langue charger a chaque page
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,9 @@ public class LoginPage extends AppCompatActivity {
         getSupportActionBar().setLogo(R.drawable.ic_launcher);
         getSupportActionBar().setDisplayShowTitleEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF6C7CE2")));
+        LocaleHelper.OnCreate(this,"en");
+        LocaleHelper.setLocale(this,"en");
+        context = this;
     }
     public void login(View view) {
         String admin1="helder";
@@ -38,13 +50,22 @@ public class LoginPage extends AppCompatActivity {
         if(log.equals(admin1)||log.equals(admin2)){
             Intent intentAdmin = new Intent(this,admin_page.class);
             startActivity(intentAdmin);
-            Toast.makeText(getApplicationContext(),"Welcome "+log+" to the Admin Page.",Toast.LENGTH_SHORT).show();
+            if(LocaleHelper.getLanguage(context)!="fr"){
+                Toast.makeText(getApplicationContext(),"Welcome "+log+" to the Admin Page.",Toast.LENGTH_SHORT).show();}
+            else{Toast.makeText(getApplicationContext(),"Bienvenue "+log+" a la page administrative.",Toast.LENGTH_SHORT).show();}
         }else if(log.equals(driver)){
             Intent intentDriver = new Intent(this,driver_page.class);
             startActivity(intentDriver);
-            Toast.makeText(getApplicationContext(),"Welcome "+log+" to your Driver Page.",Toast.LENGTH_SHORT).show();
+            if(LocaleHelper.getLanguage(context)!="fr"){
+                Toast.makeText(getApplicationContext(),"Welcome "+log+" to your Driver Page.",Toast.LENGTH_SHORT).show();
+                }
+            else{Toast.makeText(getApplicationContext(),"Bienvenue "+log+" a votre page chauffeur.",Toast.LENGTH_SHORT).show();}
         }else{
+            if(LocaleHelper.getLanguage(context)!="fr"){
                 Toast.makeText(getApplicationContext(), "Your Username or Password is not correct!", Toast.LENGTH_SHORT).show();
+
+            }else{Toast.makeText(getApplicationContext(), "Votre nom et/ou votre mot de passe n'est pas correct!", Toast.LENGTH_SHORT).show();
+                }
         }
     }
 
@@ -62,15 +83,32 @@ public class LoginPage extends AppCompatActivity {
 
         switch(item.getItemId()) {
             case R.id.id_enFlag:
-
+                LocaleHelper.setLocale(this,"en");
+                updateViews();
                 break;
             case R.id.id_frFlag:
-
+                LocaleHelper.setLocale(this,"fr");
+                updateViews();
+                break;
+            default:
+                LocaleHelper.setLocale(this,"en");
+                updateViews();
                 break;
         }
         return true;
         }
 
+    private void updateViews() {
+        Resources resources = getResources();
+
+        EditText logon = (EditText)findViewById(R.id.Log_on);
+        EditText pass = (EditText)findViewById(R.id.Log_pass);
+        Button log =(Button)findViewById(R.id.log_button);
+
+        logon.setHint(resources.getString(R.string.enter_your_username));
+        pass.setHint(resources.getString(R.string.password));
+        log.setText(resources.getString(R.string.login));
+    }
 
 
 }
