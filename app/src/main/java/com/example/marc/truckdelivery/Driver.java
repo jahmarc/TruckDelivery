4f9ee37178103b0af05640baba6cd61389351496
Helder <Osheed_@hotmail.com>
2016-11-22 19:49:37 +0100

    nouveau driver update

--- app/src/main/java/com/example/marc/truckdelivery/Driver.java
+++ app/src/main/java/com/example/marc/truckdelivery/Driver.java
@@ -5,14 +5,11 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import db.object.DriverObject;
 
 public class Driver extends AppCompatActivity {
 
--- app/src/main/java/com/example/marc/truckdelivery/DriverAdd.java
+++ app/src/main/java/com/example/marc/truckdelivery/DriverAdd.java
@@ -1,7 +1,14 @@
 package com.example.marc.truckdelivery;
 
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DriverAdd extends AppCompatActivity {
 
@@ -9,6 +16,59 @@ public class DriverAdd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_add);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_launcher);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF6C7CE2")));
+        updateViews();
+    }
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_basic, menu);
+        return true; //prends le style pour le menu de menu_search
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+
+        switch(item.getItemId()) {
+            case R.id.id_enFlag:
+                LocaleHelper.setLocale(this,"en");
+                updateViews();
+                break;
+            case R.id.id_frFlag:
+                LocaleHelper.setLocale(this,"fr");
+                updateViews();
+                break;
+            default:
+                LocaleHelper.setLocale(this,"en");
+                updateViews();
+                break;
+        }
+        return true;
+    }
+    private void updateViews() {
+        Resources resources = getResources();
+
+        EditText EditTextdr_sNom=(EditText)findViewById(R.id.editTextdrNom) ;
+        EditText editTextdr_sPrenom=(EditText)findViewById(R.id.editTextdrPrenom) ;
+        EditText editTextdr_sPlaque=(EditText)findViewById(R.id.editTextdrPlaque) ;
+        EditText editTextdr_sPhone=(EditText)findViewById(R.id.editTextdrPhone) ;
+        EditText editTextdr_sPassword=(EditText)findViewById(R.id.editTextdr_sPassword);
+        Button drbutton_sSave =(Button)findViewById(R.id.drbutton_sSave);
+
+
+
+        EditTextdr_sNom.setHint(resources.getString(R.string.nom));
+        editTextdr_sPrenom.setHint(resources.getString(R.string.pr_nom));
+        editTextdr_sPlaque.setHint(resources.getString(R.string.plaque));
+        editTextdr_sPhone.setHint(resources.getString(R.string.t_l_phone));
+        editTextdr_sPassword.setHint(resources.getString(R.string.password));
+        drbutton_sSave.setHint(resources.getString(R.string.sauvegarder));
+
+
 
     }
 }
