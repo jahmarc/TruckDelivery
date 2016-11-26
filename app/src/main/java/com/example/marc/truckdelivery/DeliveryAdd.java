e1eb4827e93a61700c252040ba8b77960e36e853
jahmarc <marc@datanet-ch.com>
2016-11-26 16:27:43 +0100

    Partie Driver List

--- app/src/main/java/com/example/marc/truckdelivery/DeliveryAdd.java
+++ app/src/main/java/com/example/marc/truckdelivery/DeliveryAdd.java
@@ -11,19 +11,28 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import Fragment.DatePickerFragment;
+import db.adapter.CustomerDataSource;
 import db.adapter.DeliveryDataSource;
+import db.adapter.DriverDataSource;
+import db.object.CustomerObject;
+import db.object.DriverObject;
 
-public class DeliveryAdd extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+public class DeliveryAdd extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, AdapterView.OnItemSelectedListener {
 
     Spinner spinnerClient_add;
     Spinner spinnerChauffeur_add;
@@ -41,6 +50,49 @@ public class DeliveryAdd extends AppCompatActivity implements DatePickerDialog.O
         setContentView(R.layout.activity_delivery_add);
 
         dts = new DeliveryDataSource(this);
+
+        // Spinner element
+        spinnerClient_add = (Spinner)findViewById(R.id.spinnerChauffeur_add);
+        spinnerChauffeur_add = (Spinner)findViewById(R.id.spinnerClient_add);
+
+        // Spinner click listener
+        spinnerClient_add.setOnItemSelectedListener(this);
+        spinnerChauffeur_add.setOnItemSelectedListener(this);
+
+        //Fill the Spinners
+        //Spinner Driver
+        DriverDataSource dts = new DriverDataSource(this);
+        List<DriverObject> drivers = new ArrayList<DriverObject>();
+
+        drivers = dts.getAllDrivers();
+        List<String> spinnerdriver =new ArrayList<String>(drivers.size());
+
+        for(DriverObject driver : drivers){
+            spinnerdriver.add(driver.getId(), driver.getFirstname()+ " " + driver.getName());
+        }
+
+        //Spinner Customer
+        CustomerDataSource cds = new CustomerDataSource(this);
+        List<CustomerObject> customers = new ArrayList<CustomerObject>();
+
+        customers = cds.getAllCustomers();
+        List<String> spinnercustomer =new ArrayList<String>();
+
+        for(CustomerObject customer : customers){
+            spinnercustomer.add(customer.getId(),customer.getSociety() + " " + customer.getFirstname()+ " " + customer.getName());
+        }
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerdriver);
+        ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnercustomer);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinnerChauffeur_add.setAdapter(dataAdapter);
+        spinnerClient_add.setAdapter(dataAdapter2);
 
         /**
          * Add additional functions to actionbar
@@ -95,6 +147,23 @@ public class DeliveryAdd extends AppCompatActivity implements DatePickerDialog.O
         buttonSave_add = (Button)findViewById(R.id.buttonSave_add);
 
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        // On selecting a spinner item
+        String item = adapterView.getItemAtPosition(i).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(adapterView.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+
     @Override
     public void onDateSet(DatePicker view, int year, int month, int day) {
         Calendar cal = new GregorianCalendar(year, month, day);
--- app/src/main/java/com/example/marc/truckdelivery/dr_delivery.java
+++ app/src/main/java/com/example/marc/truckdelivery/dr_delivery.java
@@ -1,5 +1,6 @@
 package com.example.marc.truckdelivery;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
@@ -8,13 +9,30 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import db.adapter.DeliveryDataSource;
+import db.object.DeliveryObject;
 
 public class dr_delivery extends AppCompatActivity {
+
+    Context context;
+    Bundle bundle;
+    Integer RDeliveryID;
+    DeliveryDataSource dts;
+    DeliveryObject delivery;
+    TextView dr_delivery_course;
+    TextView dr_delivery_date;
+    TextView dr_delivery_quantity;
+    TextView dr_delivery_conditioning;
+    TextView dr_delivery_commodity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dr_delivery);
+        context = this;
 
         /**
          * Add additional functions to actionbar
@@ -26,6 +44,38 @@ public class dr_delivery extends AppCompatActivity {
         actionBar.setLogo(R.drawable.ic_launcher);
         actionBar.setDisplayShowTitleEnabled(true);
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FF6C7CE2")));
+
+        dts = new DeliveryDataSource(this);
+
+        /*
+         *Recuperation des valeurs en sécurité
+         */
+        if(savedInstanceState==null) {
+            bundle = getIntent().getExtras();
+            if (bundle == null) {
+                RDeliveryID = null ;
+            } else {
+                RDeliveryID = bundle.getInt("idDelivery") ;
+            }
+        }else{
+            RDeliveryID = (int)savedInstanceState.getSerializable("idDelivery");
+        }
+
+
+        //Get the Delivery and fill the textviews
+        delivery = dts.getDeliveryById(RDeliveryID);
+
+        dr_delivery_course = (TextView)findViewById(R.id.dr_delivery_course);
+        dr_delivery_course.setText(delivery.getId());
+        dr_delivery_date = (TextView)findViewById(R.id.dr_delivery_date);
+        dr_delivery_date.setText(delivery.getDate());
+        dr_delivery_quantity = (TextView)findViewById(R.id.dr_delivery_quantity);
+        dr_delivery_quantity.setText(delivery.getQuantity());
+        dr_delivery_conditioning = (TextView)findViewById(R.id.dr_delivery_conditioning);
+        dr_delivery_conditioning.setText(delivery.getConditioning());
+        dr_delivery_commodity = (TextView)findViewById(R.id.dr_delivery_commodity);
+        dr_delivery_commodity.setText(delivery.getArticle());
+
     }
     public boolean onOptionsItemSelected(MenuItem item)
     {
